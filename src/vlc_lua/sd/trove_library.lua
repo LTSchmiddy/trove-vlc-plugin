7603224f6dc1b9c62388fa68720e7ae3c77a2207
LT_Schmiddy (Alex Schmid) <schmidalex1996@gmail.com>
2023-07-14 14:07:45 -0400

    initial commit - lua plugins loading

---
+++ src/vlc_lua/sd/trove_library.lua
@@ -0,0 +1,21 @@
+---@diagnostic disable: lowercase-global
+
+function descriptor()
+    return {
+        title = "Trove Media Library",
+        version = "1.0",
+        author = "",
+        url = 'http://',
+        capabilities = {"search"}
+    }
+end
+
+function main()
+    
+    -- Do stuff here
+  end
+  
+  function search(query)
+    
+    -- Do stuff here
+  end
