4d6efca85b6e67111ca280a3d6e8cb2586c011fe
LT_Schmiddy (Alex Schmid) <schmidalex1996@gmail.com>
2023-07-23 20:10:02 -0400

    Lua logging now supports all Lua object types

--- src/cpp/scripting/extensions/lua_log.cpp
+++ src/cpp/scripting/extensions/lua_log.cpp
@@ -1,27 +1,53 @@
+#include <format>
+#include <plog/Log.h>
 #include "lua_log.h"
 
-
-#include <plog/Log.h>
-
 namespace Scripting::Extensions::Logging {
-    
-    static void _lua_log_verbose(std::string val) {
-        PLOGV << val;
+
+    static std::string _lua_log_format_for_log (sol::object val) {
+        switch (val.get_type()) {
+            case sol::type::none:
+                return std::format("*Nothing*");
+            case sol::type::lua_nil:
+                return std::format("nil");
+            case sol::type::string:
+                return std::format("\'{}\'", val.as<std::string>());
+            case sol::type::number:
+                return std::format("{}", val.as<float>());
+            case sol::type::boolean:
+                return std::format("{}", val.as<bool>());
+            case sol::type::function:
+                return std::format("Function at address '{}'", val.pointer());
+            case sol::type::userdata:
+                return std::format("Userdata at address {}", val.pointer());
+            case sol::type::lightuserdata:
+                return std::format("Lightuserdata at address '{}'", val.pointer());
+            case sol::type::table:
+                return std::format("Table at address '{}'", val.pointer());
+            case sol::type::poly:
+                return std::format("Poly at address '{}'", val.pointer());
+            default:
+                return std::format("Unknown object at address '{}'", val.pointer());
+        }
+
     }
-    static void _lua_log_debug(std::string val) {
-        PLOGD << val;
+    static void _lua_log_verbose(sol::object val) {
+        PLOGV << _lua_log_format_for_log(val);
     }
-    static void _lua_log_info(std::string val) {
-        PLOGI << val;
+    static void _lua_log_debug(sol::object val) {
+        PLOGD << _lua_log_format_for_log(val);
     }
-    static void _lua_log_warning(std::string val) {
-        PLOGW << val;
+    static void _lua_log_info(sol::object val) {
+        PLOGI << _lua_log_format_for_log(val);
     }
-    static void _lua_log_error(std::string val) {
-        PLOGE << val;
+    static void _lua_log_warning(sol::object val) {
+        PLOGW << _lua_log_format_for_log(val);
     }
-    static void _lua_log_fatal(std::string val) {
-        PLOGF << val;
+    static void _lua_log_error(sol::object val) {
+        PLOGE << _lua_log_format_for_log(val);
+    }
+    static void _lua_log_fatal(sol::object val) {
+        PLOGF << _lua_log_format_for_log(val);
     }
 
     void create_lua_log_module(sol::state_view* lua) {
