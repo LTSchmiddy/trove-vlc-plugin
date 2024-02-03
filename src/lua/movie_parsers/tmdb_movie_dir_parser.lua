40da0ca00586b98775ef110229226dad087d74af
LT_Schmiddy (Alex Schmid) <schmidalex1996@gmail.com>
2024-02-02 23:55:16 -0500

    removed extra text from movie parser

--- src/lua/movie_parsers/tmdb_movie_dir_parser.lua
+++ src/lua/movie_parsers/tmdb_movie_dir_parser.lua
@@ -1,16 +1,9 @@
--- path = "House.S01E01.720p.BluRay.x264.300MB-Pahe.in.mkv"
--- expr = "(S\\d+E\\d+)"
--- regex_out = {}
--- regex.search_iter(path, expr, regex_out, {}, {})
--- print(cjson.encode(regex_out))
-
 string = require("string")
 
 function string_trim(s)
     return string.gsub(s,"^%s*(.-)%s*$", "%1")
 end
 
-log.debug("Hello")
 
 -- REQUIRED FUNCTIONS
 function year_parse_method(str, retVal)
